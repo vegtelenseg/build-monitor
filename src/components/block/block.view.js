4969744ea1d27edaf82e61147f401da6cb190397
Siya <dot.netntoz@gmail.com>
2018-06-15 04:28:33 +0200

    Made box and healthe-checker pure components

--- src/components/block/block.view.js
+++ src/components/block/block.view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class BlockView extends React.Component {
+export default class BlockView extends React.PureComponent {
     state = {
         isVisible: false
     };
